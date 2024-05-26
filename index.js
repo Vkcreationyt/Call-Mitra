d5e85b0a6bf97e415e533a2c0cb1e692f43bc874
Vkcreationyt <110219805+Vkcreationyt@users.noreply.github.com>
2024-05-26 11:17:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-hi
+// Function to log online status
+function userIsOnline() {
+  console.log('User is online');
+}
+
+// Event listener for online event
+window.addEventListener('online', userIsOnline);
+
+console.lo('online');
+
+
+
